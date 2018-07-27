893be728e41a12dfca7fc8f7a87d44ca69b7c5f8
이성희 <>
2018-07-28 02:55:10 +0900

    페이지 상세 추가

--- web_script/web_skill.js
+++ web_script/web_skill.js
@@ -17,8 +17,12 @@ module.exports = function(app){
   route.get('/', function (request, response) {
     // id값이 존재하는 경우, 상세 페이지로 이동
     if (request.query.id != undefined && request.query.id != ''){
-      skillDetailPage(request.query.id, request, response);
-      return;
+      for (var i = 0; i < skillTable.length; i ++){
+        if (skillTable[i].ClassID.indexOf(request.query.id) > -1){
+          skillDetailPage(i, request, response);
+          return;
+        }
+      }
     }
 
     // string query에 검색 데이터가 있는 경우, 검색 결과 가져옴.
@@ -64,8 +68,51 @@ module.exports = function(app){
     //console.log(request.query.searchType + " " + request.query.searchName);
   });
 
-  function skillDetailPage(classId, request, response) {
-    response.send(classId);
+  function skillDetailPage(index, request, response) {
+    var output = '<html>';
+    output +=   '<head>';
+    output +=     '<title>' + skillTable[index].Name + '</title>';
+    output +=     '<link rel="stylesheet" type="text/css" href="../style.css">';
+    output +=   '</head>';
+    output +=   '<body>';
+    output +=     '<h1>' + skillTable[index].Name + '</h1>';
+    output +=     '<p>' + skillTable[index].EngName + '</p>';
+    output +=     '<table style="width:100%">';
+    output +=       '<tr>';
+    output +=         '<td>ClassId</td>';
+    output +=         '<td>ClassName</td>';
+    output +=         '<td>Job</td>';
+    output +=         '<td>Rank</td>';
+    output +=       '</tr>';
+    output +=       '<tr>';
+    output +=         '<td>' + skillTable[index].ClassID + '</td>';
+    output +=         '<td>' + skillTable[index].ClassName + '</td>';
+    output +=         '<td>' + skillTable[index].Job + '</td>';
+    output +=         '<td>' + skillTable[index].Rank + '</td>';
+    output +=       '</tr>';
+    output +=     '</table>';
+    output +=     '<table style="width:100%">';
+    output +=       '<tr>';
+    output +=         '<td>ClassType</td>';
+    output +=         '<td>ValueType</td>';
+    output +=         '<td>Attribute</td>';
+    output +=         '<td>AttackType</td>';
+    output +=         '<td>HitType</td>';
+    output +=       '</tr>';
+    output +=       '<tr>';
+    output +=         '<td>' + skillTable[index].ClassType + '</td>';
+    output +=         '<td>' + skillTable[index].ValueType + '</td>';
+    output +=         '<td>' + skillTable[index].Attribute + '</td>';
+    output +=         '<td>' + skillTable[index].AttackType + '</td>';
+    output +=         '<td>' + skillTable[index].HitType + '</td>';
+    output +=       '</tr>';
+    output +=     '</table>';
+    output +=     '<p>' + skillTable[index].Caption + '</p>';
+    output +=     '<p>' + skillTable[index].Caption2 + '</p>';
+    output +=   '</body>';
+    output += '</html>';
+
+    response.send(output);
   }
 
   return route;
