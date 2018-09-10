c3d9cfcca04fd91773927f236b00452af77e55e1
이성희 <>
2018-09-10 21:33:52 +0900

    캡션에 색상/아이콘 기능 추가

--- my_modules/TosModule.js
+++ my_modules/TosModule.js
@@ -3,6 +3,13 @@ class TosModule {
     static parseCaption(caption) {
         var output = caption;
         output = output.replace(/{np}|{nl}/g, '<br/>');
+        output = output.replace(/{img tooltip_speedofatk}/g, '<img src="../img/tooltip_speedofatk.png" style="height:1em; vertical-align:middle;" />');
+        output = output.replace(/{#DD5500}/g, '<span style="color:#DD5500;">');
+        output = output.replace(/{#7AE4FF}/g, '<span style="color:#7AE4FF;">');
+        output = output.replace(/{#993399}/g, '<span style="color:#993399;">');
+        output = output.replace(/{ol}/g, '<span style="font-weight:bold;">');
+        output = output.replace(/{\/}/g, '</span>');
+
         output = output.replace(/#{SkillSR}#/g, '<span id="SkillSR"></span>');
         output = output.replace(/#{SkillFactor}#/g, '<span id="SkillFactor"></span>');
         output = output.replace(/#{CaptionTime}#/g, '<span id="CaptionTime"></span>');
