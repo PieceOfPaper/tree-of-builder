59d7408431816a00e6fd5b192ed08d1600fe4f30
이성희 <>
2018-09-13 01:53:10 +0900

    아이템 정보 페이지 - 기본 구성

--- web_script/web_item.js
+++ web_script/web_item.js
@@ -24,9 +24,39 @@ module.exports = function(app, tableData, scriptData){
 
     // id값이 존재하는 경우, 상세 페이지로 이동
     if (request.query.id != undefined && request.query.id != ''){
-      for (var i = 0; i < abilityTable.length; i ++){
-        if (abilityTable[i].ClassID === request.query.id){
-          //itemDetailPage(i, request, response);
+      for (var i = 0; i < itemTable.length; i ++){
+        if (itemTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
+          return;
+        }
+      }
+      for (var i = 0; i < itemEquipTable.length; i ++){
+        if (itemEquipTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
+          return;
+        }
+      }
+      for (var i = 0; i < itemPremiumTable.length; i ++){
+        if (itemPremiumTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
+          return;
+        }
+      }
+      for (var i = 0; i < itemQuestTable.length; i ++){
+        if (itemQuestTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
+          return;
+        }
+      }
+      for (var i = 0; i < itemGemTable.length; i ++){
+        if (itemGemTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
+          return;
+        }
+      }
+      for (var i = 0; i < itemRecipeTable.length; i ++){
+        if (itemRecipeTable[i].ClassID === request.query.id){
+          itemDetailPage(i, request, response);
           return;
         }
       }
@@ -153,8 +183,8 @@ module.exports = function(app, tableData, scriptData){
     var resultString = '';
     for (var i = 0; i < resultArray.length; i ++){
       resultString += '<tr>';
-      //resultString += '<td align="center"><a href="?id=' + resultArray[i].ClassID + '">' + resultArray[i].ClassID + '</a></td>';
-      resultString += '<td align="center">' + resultArray[i].ClassID + '</td>';
+      resultString += '<td align="center"><a href="?id=' + resultArray[i].ClassID + '">' + resultArray[i].ClassID + '</a></td>';
+      //resultString += '<td align="center">' + resultArray[i].ClassID + '</td>';
       // 공용 코스튬은 아이콘이 두개
       if (resultArray[i].EqpType != undefined && resultArray[i].UseGender != undefined && 
           resultArray[i].EqpType.toLowerCase() == 'outer' && resultArray[i].UseGender.toLowerCase() == 'both'){
@@ -186,176 +216,43 @@ module.exports = function(app, tableData, scriptData){
     response.send(output);
   });
 
-  var layout_detail = fs.readFileSync('./web/Layout/index-abilitydetail.html');
+  var layout_item_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
+  var layout_itemEquip_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
+  var layout_itemPremium_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
+  var layout_itemQuest_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
+  var layout_itemGem_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
+  var layout_itemRecipe_detail = fs.readFileSync('./web/Layout/index-itemdetail.html');
 
   function itemDetailPage(index, request, response) {
-    var skillTable = tableData['skill'];
-    var abilityTable = tableData['ability'];
-    //var skillTreeTable = tableData['skilltree'];
-    //var jobTable = tableData['job'];
+    var itemTable = tableData['item'];
 
-    var abilityJob;
-    for (var i = 0; i < tableData['ability_job'].length; i ++){
-      if (tableData['ability_job'][i].ClassName === abilityTable[index].ClassName){
-        abilityJob = tableData['ability_job'][i];
-        break;
-      }
-    }
-
-    // var skillMaxLevel = 1;
-    // for (var i = 0; i < skillTreeTable.length; i ++){
-    //   if (skillTreeTable[i].SkillName == abilityTable[index].ClassName){
-    //     skillMaxLevel = skillTreeTable[i].MaxLevel;
-    //     break;
-    //   }
-    // }
-
-    var captionScript = '';
-    captionScript += '<script>';
-
-    captionScript += 'function GetSkillOwner(skill){'
-    captionScript += 'var playerSetting = {';
-    captionScript +=  'Level:Number(1),';
-    captionScript +=  'SR:Number(3),';
-    captionScript +=  'STR:Number(0),';
-    captionScript +=  'CON:Number(0),';
-    captionScript +=  'INT:Number(0),';
-    captionScript +=  'SPR:Number(0),';
-    captionScript +=  'DEX:Number(0),';
-    captionScript += '};';
-    captionScript += 'return playerSetting; }';
-
-    captionScript += 'function GetAbility(pc, ability){ return undefined; }';
-    captionScript += 'function TryGetProp(data, prop){ return data[prop]; }';
-    captionScript += 'function IsBuffApplied(pc, buff){ return false; }';
-    captionScript += 'function IGetSumOfEquipItem(pc, equip){ return 0; }';
-    captionScript += 'function IsPVPServer(pc){ return 0; }';
-
-    captionScript += 'var abilLvPrev = Number(0);';
-    captionScript += 'var abilLvNext = Number(1);';
-
-    if (abilityJob != undefined){
-      captionScript += 'document.getElementById("AbilityLevelPrev").min=0;';
-      captionScript += 'document.getElementById("AbilityLevelPrev").max=' + abilityJob.MaxLevel + ';';
-      captionScript += 'document.getElementById("AbilityLevelNext").min=0;';
-      captionScript += 'document.getElementById("AbilityLevelNext").max=' + abilityJob.MaxLevel + ';';
-    }
-
-    captionScript += 'onChangeAbilityLevel();';
-
-    captionScript += 'function onChangeAbilityLevel(){';
-    captionScript +=  'abilLvPrev = document.getElementById("AbilityLevelPrev").value;';
-    captionScript +=  'abilLvNext = document.getElementById("AbilityLevelNext").value;';
-    captionScript +=  'updateLuaScripts();';
-    captionScript += '}';
-
-    captionScript += 'function onClickLevelUpPrev(){';
-    captionScript +=  'abilLvPrev ++;';
-    captionScript +=  'if (abilLvPrev > document.getElementById("AbilityLevelPrev").max) abilLvPrev = document.getElementById("AbilityLevelPrev").max;';
-    captionScript +=  'document.getElementById("AbilityLevelPrev").value = abilLvPrev;';
-    captionScript +=  'updateLuaScripts();';
-    captionScript += '}';
-
-    captionScript += 'function onClickLevelDownPrev(){';
-    captionScript +=  'abilLvPrev --;';
-    captionScript +=  'if (abilLvPrev < document.getElementById("AbilityLevelPrev").min) abilLvPrev = document.getElementById("AbilityLevelPrev").min;';
-    captionScript +=  'document.getElementById("AbilityLevelPrev").value = abilLvPrev;';
-    captionScript +=  'updateLuaScripts();';
-    captionScript += '}';
-
-    captionScript += 'function onClickLevelUpNext(){';
-    captionScript +=  'abilLvNext ++;';
-    captionScript +=  'if (abilLvNext > document.getElementById("AbilityLevelNext").max) abilLvNext = document.getElementById("AbilityLevelNext").max;';
-    captionScript +=  'document.getElementById("AbilityLevelNext").value = abilLvNext;';
-    captionScript +=  'updateLuaScripts();';
-    captionScript += '}';
-
-    captionScript += 'function onClickLevelDownNext(){';
-    captionScript +=  'abilLvNext --;';
-    captionScript +=  'if (abilLvNext < document.getElementById("AbilityLevelNext").min) abilLvNext = document.getElementById("AbilityLevelNext").min;';
-    captionScript +=  'document.getElementById("AbilityLevelNext").value = abilLvNext;';
-    captionScript +=  'updateLuaScripts();';
-    captionScript += '}';
-
-    captionScript += 'function updateLuaScripts(){';
-    if (abilityJob != undefined){
-      captionScript += 'var price = Number(0);';
-      captionScript += 'for (var i = Number(abilLvPrev + 1); i <= abilLvNext; i ++){';
-      captionScript +=  'price+=Number(' + abilityJob.ScrCalcPrice + '(undefined,"' + abilityTable[index].ClassName + '",i,' + abilityJob.MaxLevel + '));';
-      captionScript += '}';
-      captionScript += 'if (document.getElementById("PricePoint") != undefined) document.getElementById("PricePoint").innerHTML=price;';
-    }
-    captionScript += '}';
-
-    if (abilityJob != undefined){
-      captionScript += tos.Lua2JS(scriptData[abilityJob.ScrCalcPrice]).replace('return price, time', 'return price');
-    }
-    
-    captionScript += '</script>';
-
-    
-    var jobsString = '';
-    if (abilityTable[index].Job != undefined){
-      var splited = abilityTable[index].Job.split(';');
-      for (var i = 0; i < splited.length; i ++){
-        jobsString += tos.JobClassNameToJobName(tableData, splited[i]);
-        if ((i+1) < splited.length) jobsString += ', ';
-      }
-    }
-
-    var skillString = '';
-    if (abilityTable[index].SkillCategory != undefined){
-      for (var i = 0; i < skillTable.length; i ++){
-        if (skillTable[i].ClassName === abilityTable[index].SkillCategory){
-          skillString = '<a href="../Skill/?id=' + skillTable[i].ClassID + '">' + skillTable[i].Name + '</a>';
-          break;
-        }
-      }
-    }
-
-    var spendString = '';
-    if (abilityTable[index].AddSpend != undefined){
-      var splited = abilityTable[index].AddSpend.split('/');
-      for (var i = 0; i < splited.length; i += 2) {
-        if (splited[i] === 'SP'){
-          spendString += 'SP+';
-          if ((i + 1) < splited.length) spendString += splited[i+1] + '%';
-        } else if (splited[i] === 'CoolDown'){
-          spendString += 'CoolDown';
-          if ((i + 1) < splited.length) {
-            if (Number(splited[i+1]) > 0) spendString += '+' + (Number(splited[i+1])/1000) + 'Sec';
-            else spendString += (Number(splited[i+1])/1000) + 's';
-          }
-        }
-        
-        if ((i + 2) < splited.length) spendString += '<br/>';
-      }
-    }
-
-    var output = layout_detail.toString();
+    var output = layout_item_detail.toString();
     output = output.replace(/style.css/g, '../Layout/style.css');
-    output = output.replace(/%Icon%/g, '<img src="../img/icon/skillicon/' + abilityTable[index].Icon + '.png" />');
-    output = output.replace(/%Name%/g, abilityTable[index].Name);
-    output = output.replace(/%ClassName%/g, abilityTable[index].ClassName);
-    output = output.replace(/%ClassID%/g, abilityTable[index].ClassID);
-    output = output.replace(/%SkillCategory%/g, skillString);
-    output = output.replace(/%ActiveState%/g, abilityTable[index].ActiveState);
-    output = output.replace(/%AddSpend%/g, spendString);
-    output = output.replace(/%AlwaysActive%/g, abilityTable[index].AlwaysActive);
-    output = output.replace(/%IsEquipItemAbil%/g, abilityTable[index].IsEquipItemAbil);
-
-    if (abilityJob === undefined){
-      output = output.replace(/%MaxLevel%/g, 0);
-      output = output.replace(/%UnlockDesc%/g, '');
+    output = output.replace(/%Icon%/g, '<img src="../img/icon/itemicon/' + itemTable[index].Icon.toLowerCase() + '.png" />');
+    if (itemTable[index].GroupName.toLowerCase() == 'card'){
+      output = output.replace(/%TooltipImage%/g, '<img src="../img/bosscard2/' + itemTable[index].TooltipImage.toLowerCase() + '.png" />');
     } else {
-      output = output.replace(/%MaxLevel%/g, abilityJob.MaxLevel);
-      output = output.replace(/%UnlockDesc%/g, abilityJob.UnlockDesc);
+      output = output.replace(/%TooltipImage%/g, '<img src="../img/icon/itemicon/' + itemTable[index].TooltipImage.toLowerCase() + '.png" />');
     }
+    output = output.replace(/%Name%/g, itemTable[index].Name);
+    output = output.replace(/%ClassName%/g, itemTable[index].ClassName);
+    output = output.replace(/%ClassID%/g, itemTable[index].ClassID);
 
-    output = output.replace(/%Desc%/g, tos.parseCaption(abilityTable[index].Desc));
-    output = output.replace(/%Jobs%/g, jobsString);
+    output = output.replace(/%ItemType%/g, itemTable[index].ItemType);
+    output = output.replace(/%Journal%/g, itemTable[index].Journal);
+    output = output.replace(/%GroupName%/g, itemTable[index].GroupName);
+    output = output.replace(/%Weight%/g, itemTable[index].Weight);
+    output = output.replace(/%MaxStack%/g, itemTable[index].MaxStack);
+    output = output.replace(/%CardGroupName%/g, itemTable[index].CardGroupName);
 
-    output = output.replace(/%AddCaptionScript%/g, captionScript);
+    output = output.replace(/%MaterialPrice%/g, itemTable[index].MaterialPrice);
+    output = output.replace(/%Price%/g, itemTable[index].Price);
+    output = output.replace(/%PriceRatio%/g, itemTable[index].PriceRatio);
+    output = output.replace(/%SellPrice%/g, itemTable[index].SellPrice);
+    output = output.replace(/%RepairPriceRatio%/g, itemTable[index].RepairPriceRatio);
+
+    output = output.replace(/%Desc%/g, tos.parseCaption(itemTable[index].Desc));
+    output = output.replace(/%Desc_Sub%/g, tos.parseCaption(itemTable[index].Desc_Sub));
 
     output = output.replace(/%AddTopMenu%/g, layout_topMenu.toString());
 
