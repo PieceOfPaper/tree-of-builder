610171e1e6ce62a4062326c14b73eab036f3ef0d
이성희 <>
2018-11-02 18:28:40 +0900

    [데이터 서버 분리] 스킬 페이지 리뉴얼 - 검색필터 쿼리스트링에 저장되도록 수정

--- web/js/requestMethod.js
+++ web/js/requestMethod.js
@@ -4,16 +4,7 @@ function requestGetData(tableName, filter, callback){
     var xmlhttp = new XMLHttpRequest();
     var url = "../data/" + tableName;
 
-    var queryString = "";
-    if (filter != undefined){
-        var index = 0;
-        for (var param in filter) {
-            if (index == 0) queryString += "?";
-            else queryString += "&";
-            queryString += param + "=" + filter[param];
-            index ++;
-        }
-    }
+    var queryString = filterToQueryStr(filter);
     
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -24,4 +15,35 @@ function requestGetData(tableName, filter, callback){
 
     xmlhttp.open("GET", url + queryString, true);
     xmlhttp.send();
+}
+
+function filterToQueryStr(filter){
+    var queryString = "";
+    if (filter != undefined){
+        var index = 0;
+        for (var param in filter) {
+            if (index == 0) queryString += "?";
+            else queryString += "&";
+            queryString += param + "=" + filter[param];
+            index ++;
+        }
+    }
+    return queryString;
+}
+
+function urlToFilter(url){
+    var splited = url.split('?');
+    if (splited < 2) return [];
+    return queryStrToFilter(splited[1]);
+}
+
+function queryStrToFilter(query){
+    var filter = [];
+    if (query==undefined) return filter;
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        filter[pair[0]] = pair[1];
+    }
+    return filter;
 }
