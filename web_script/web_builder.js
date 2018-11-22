13aec7a18396e51be1439b243b99b61738e8ddb1
이성희 <>
2018-11-22 17:29:07 +0900

    빌더 페이지 - 특성 적용

--- web_script/web_builder.js
+++ web_script/web_builder.js
@@ -24,6 +24,7 @@ module.exports = function(app, tableData, scriptData){
         var classArray = [];
         var skillArray = [];
         var classCount = [];
+        var abilityArray = [];
         if (request.query.class != undefined && request.query.class.length > 0){
             for (var i = 0; i < jobTable.length; i ++){
                 classCount.push(0);
@@ -49,6 +50,22 @@ module.exports = function(app, tableData, scriptData){
                     skill.push(AsciiToNumber(splited_query_skill[i][j]));
                 }
                 skillArray.push(skill);
+            }
+        }
+        if (request.query.ability != undefined && request.query.ability.length > 0){
+            var splited_query_ability = request.query.ability.split(".");
+            for (var i = 0; i < splited_query_ability.length; i ++){
+                if (splited_query_ability[i].length == 0) continue;
+                var ability = [];
+                ability.push(AsciiToNumber(splited_query_ability[i][0]));
+                for (var j = 1; j < splited_query_ability[i].length; j += 3){
+                    ability.push(AsciiToNumber(splited_query_ability[i][j]));
+
+                    var ten = AsciiToNumber(splited_query_ability[i][j + 1]) * getNumberAsciiMax();
+                    var one = AsciiToNumber(splited_query_ability[i][j + 2]);
+                    ability.push(ten + one);
+                }
+                abilityArray.push(ability);
             }
         }
 
@@ -110,7 +127,7 @@ module.exports = function(app, tableData, scriptData){
         output +=       '<hr>';
         output +=       '<button id="viewDetailBtn" onclick="onClickViewDetail()">View Details</button>';
         output +=       '<div class="builder-skill-area">';
-        output +=       '<script>var skillData=[];</script>';
+        output +=       '<script>var skillData=[]; var abilityData=[];  var abilityJobData=[];</script>';
         var skillIndex = 0;
         for (var i = 0; i < classCount.length; i ++){
             if (classCount[i] <= 0)
@@ -130,6 +147,7 @@ module.exports = function(app, tableData, scriptData){
             //output +=       '<p>(<span id="' + jobNum2 + '" class="skillLvSum">' + skillLvSum + '</span>/' + (classCount[i] * 45) + ')</p>';
             var skillPointMax = jobNum2 == 1 ? 15 : 45;
             output +=       '<p>(<span id="' + jobNum2 + '" class="skillLvSum">' + skillLvSum + '</span>/' + skillPointMax + ')</p>';
+            // ------ Skill
             for (var j = 0; j < skilltreeTable.length; j ++){
                 if (skilltreeTable[j].ClassName.indexOf(jobTable[i].ClassName + '_') > -1){
                     var skillTableIndex;
@@ -179,8 +197,8 @@ module.exports = function(app, tableData, scriptData){
                     skillIndex ++;
                 }
             }
-            output +=       '</div>';
-            output +=       '<div>';
+            // ------ Ability
+            var abilIndex = 0;
             for (var j = 0; j < abilityTable.length; j ++){
                 if (abilityTable[j].Job == jobTable[i].ClassName){
                     var abil_job = undefined;
@@ -191,18 +209,34 @@ module.exports = function(app, tableData, scriptData){
                         }
                     }
                     if (abil_job == undefined) continue;
-                    //output +=       ''+abilityTable[j].Name+', ';
-                    // output +=   '<div align="center" class="skill" id="' + abilityTable[j].ClassID + '" >';
-                    // output +=       '<img src="../img/icon/skillicon/' + abilityTable[j].Icon.toLowerCase()  + '.png"/>';
-                    // output +=       '<br>';
-                    // output +=       '<button class="lv-add-button plus" onclick="addAbilLevel(' + jobNum2 + ',' + skillIndex+ ',' + abil_job.MaxLevel + ',1)"><img src="../img/button/btn_plus_cursoron.png" /></button>';
-                    // output +=       '<button class="lv-add-button minus" onclick="addAbillLevel(' + jobNum2 + ',' + skillIndex+ ',' + abil_job.MaxLevel + ',-1)"><img src="../img/button/btn_minus_cursoron.png" /></button>';
-                    // output +=       '<p><a href="../Skill/?id=' + abilityTable[j].ClassID  + '">' + abilityTable[j].Name + '</a>(<span id="' + jobNum2 + ',' + skillIndex + '" class="skillLv">' + skillLv + '</span>/' + skillLvMax + ')</p>';
-                    // output +=       '<div align="center" class="skill-desc" id="' + abilityTable[j].ClassID + '" >';
-                    // output +=           '<p>' + tos.parseCaption(abilityTable[j].Caption) + '</p>';
-                    // output +=           '<p>' + tos.parseCaption(abilityTable[j].Caption2) + '</p>';
-                    // output +=       '</div>';
-                    // output +=   '</div>';
+                    var abilLv = 0;
+                    for (var k = 0; k < abilityArray.length; k ++){
+                        if (abilityArray[k][0] == jobNum2){
+                            for (var l = 1; l < abilityArray[k].length; l +=2){
+                                if (abilityArray[k][l] == abilIndex){
+                                    abilLv = abilityArray[k][l + 1];
+                                }
+                            }
+                        }
+                    }
+                    output +=   '<div align="center" class="ability" id="' + abilityTable[j].ClassID + '" >';
+                    output +=       '<img src="../img/icon/skillicon/' + abilityTable[j].Icon.toLowerCase()  + '.png"/>';
+                    output +=       '<br>';
+                    output +=       '<button class="lv-add-button plus" onclick="addAbilLevel(' + jobNum2 + ',' + abilIndex + ',' + abil_job.MaxLevel + ',1)"><img src="../img/button/btn_plus_cursoron.png" /></button>';
+                    output +=       '<button class="lv-add-button minus" onclick="addAbilLevel(' + jobNum2 + ',' + abilIndex + ',' + abil_job.MaxLevel + ',-1)"><img src="../img/button/btn_minus_cursoron.png" /></button>';
+                    output +=       '<p><a href="../Ability/?id=' + abilityTable[j].ClassID  + '">' + abilityTable[j].Name + '</a>(<span id="' + jobNum2 + ',' + abilIndex + '" class="abilityLv">' + abilLv + '</span>/' + abil_job.MaxLevel + ')</p>';
+                    output +=       '<div align="center" class="ability-desc" id="' + abilityTable[j].ClassID + '" >';
+                    output +=           '<p>' + abil_job.UnlockDesc + '</p>';
+                    output +=           '<p>' + tos.parseCaption(abilityTable[j].Desc) + '</p>';
+                    output +=       '</div>';
+                    output +=       '<input type="hidden" id="Ability_' + abilityTable[j].ClassName + '" class="AbilityData" min="0" max="' + abil_job.MaxLevel + '" value="0" >';
+                    output +=       '<script>';
+                    output +=           'abilityData["'+abilIndex+'"]=' + JSON.stringify(abilityTable[j]) + ';';
+                    output +=           'abilityJobData["'+abilIndex+'"]=' + JSON.stringify(abil_job) + ';';
+                    output +=           'abilityData["'+abilIndex+'"]["Level"]=Number('+abilLv+');';
+                    output +=       '</script>';
+                    output +=   '</div>';
+                    abilIndex ++;
                 }
             }
             output +=       '</div>';
@@ -237,6 +271,8 @@ module.exports = function(app, tableData, scriptData){
         output +=       'function IsPVPServer(pc){ return 0; }';
 
         output +=   'var methods=[];';
+        luaMethodList.push('SCR_ABIL_ADD_SKILLFACTOR');
+        luaMethodList.push('SCR_ABIL_ADD_SKILLFACTOR_TOOLTIP');
         luaMethodList.push('SCR_REINFORCEABILITY_TOOLTIP');
         for (var i=0;i<luaMethodList.length;i++){ 
             if (scriptData[luaMethodList[i]] == undefined) continue;
@@ -273,7 +309,7 @@ module.exports = function(app, tableData, scriptData){
         } else if (ascii >= 97 && ascii <= 122){
             return ascii - 97 + 10;
         } else if (ascii >= 65 && ascii <= 90){
-            return ascii - 65 + 26;
+            return ascii - 65 + 36;
         }
         return -1;
     }
@@ -283,9 +319,13 @@ module.exports = function(app, tableData, scriptData){
             return String.fromCharCode(48 + num);
         } else if (num >= 10 && num <= 35){
             return String.fromCharCode(97 + num - 10);
-        } else if (num >= 36 && num <= 51){
-            return String.fromCharCode(65 + num - 26);
+        } else if (num >= 36 && num <= 61){
+            return String.fromCharCode(65 + num - 36);
         }
+    }
+
+    function getNumberAsciiMax() {
+        return 61;
     }
   
     return route;
