af416b42799c67ec02ad5654a190002ba47bd2b4
이성희 <>
2018-11-23 23:28:43 +0900

    광고 삽입 - 모든 페이지에 코드 추가

--- web_script/web_builder.js
+++ web_script/web_builder.js
@@ -74,6 +74,8 @@ module.exports = function(app, tableData, scriptData){
         output +=     '<title>Builder Page</title>';
         output +=     '<link rel="stylesheet" type="text/css" href="../style.css">';
         output +=     '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />';
+        output +=     '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
+        output +=     '<script>(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-2172243042066207", enable_page_level_ads: true }) </script>';
         output +=     '<script src="../js/defaultSetting.js"></script>';
         output +=   '</head>';
         output +=   '<body>';
