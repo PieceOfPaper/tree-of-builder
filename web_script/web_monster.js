8e87f378f396feb837ebc82916cc6ec307548090
이성희 <>
2018-12-15 02:01:51 +0900

    몬스터 페이지 - 디테일 페이지 구성

--- web_script/web_monster.js
+++ web_script/web_monster.js
@@ -22,10 +22,26 @@ module.exports = function(app, tableData, scriptData){
         response.send('');
     });
   
+
+    var layout_detail = fs.readFileSync('./web/MonsterPage/detail.html');
     function monsterDetailPage(index, request, response) {
         var monsterTable = tableData['monster'];
 
-        var output = JSON.stringify(monsterTable[index]);
+        var output = layout_detail.toString();
+        output = output.replace(/%Icon%/g, '<img src="../img/icon/monillust/' + monsterTable[index].Icon.toLowerCase() + '.png" />');
+        output = output.replace(/%Name%/g, monsterTable[index].Name);
+        output = output.replace(/%ClassName%/g, monsterTable[index].ClassName);
+        output = output.replace(/%ClassID%/g, monsterTable[index].ClassID);
+        output = output.replace(/%Desc%/g, monsterTable[index].Desc);
+
+        output = output.replace(/%Level%/g, monsterTable[index].Level);
+        output = output.replace(/%Attribute%/g, monsterTable[index].Attribute);
+        output = output.replace(/%RaceType%/g, monsterTable[index].RaceType);
+        output = output.replace(/%ArmorMaterial%/g, monsterTable[index].ArmorMaterial);
+        output = output.replace(/%MoveType%/g, monsterTable[index].MoveType);
+        output = output.replace(/%Size%/g, monsterTable[index].Size);
+        output = output.replace(/%MonRank%/g, monsterTable[index].MonRank);
+
         response.send(output);
     }
   
