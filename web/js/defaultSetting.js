0907a198a7f64e28fb806b8eafe75af016cbc0c9
이성희 <>
2018-12-15 01:39:39 +0900

    몬스터 페이지 - 기본 검색 추가

--- web/js/defaultSetting.js
+++ web/js/defaultSetting.js
@@ -4,26 +4,37 @@ var topMenuButtons = [];
 topMenuButtons['home'] = [];
 topMenuButtons['home']["Path"] = ".";
 topMenuButtons['home']["Img"] = "../img/minimap_icons/questinfo_return.png";
+topMenuButtons['home']["Name"] = "HOME";
 
 topMenuButtons['item'] = [];
 topMenuButtons['item']["Path"] = "Item";
 topMenuButtons['item']["Img"] = "../img/icon/itemicon/accessory_hat_poporion.png";
+topMenuButtons['item']["Name"] = "ITEM";
 
 topMenuButtons['skill'] = [];
 topMenuButtons['skill']["Path"] = "SkillPage";
 topMenuButtons['skill']["Img"] = "../img/icon/skillicon/icon_warri_tercioformation.png";
+topMenuButtons['skill']["Name"] = "SKILL";
 
 topMenuButtons['ability'] = [];
 topMenuButtons['ability']["Path"] = "Ability";
 topMenuButtons['ability']["Img"] = "../img/icon/skillicon/ability_warrior_centurion2.png";
+topMenuButtons['ability']["Name"] = "ABIL";
 
 topMenuButtons['buff'] = [];
 topMenuButtons['buff']["Path"] = "Buff";
 topMenuButtons['buff']["Img"] = "../img/bufficon/icon_attack_blessing_up.png";
+topMenuButtons['buff']["Name"] = "BUFF";
+
+topMenuButtons['monster'] = [];
+topMenuButtons['monster']["Path"] = "MonsterPage";
+topMenuButtons['monster']["Img"] = "../img/icon/monillust/mon_popolion_blue.png";
+topMenuButtons['monster']["Name"] = "OBJ";
 
 topMenuButtons['builder'] = [];
 topMenuButtons['builder']["Path"] = "Builder";
 topMenuButtons['builder']["Img"] = "../img/hud_status_icon/skillpower_icon.png";
+topMenuButtons['builder']["Name"] = "BUILD";
 
 
 
@@ -39,7 +50,7 @@ function onInitTopMenu(isHome){
             if (isHome) nodeButton.href=topMenuButtons[param].Path;
             else nodeButton.href="../"+topMenuButtons[param].Path;
             nodeButtonImg.src=topMenuButtons[param].Img;
-            nodeButtonText.innerText=param.toUpperCase();
+            nodeButtonText.innerText=topMenuButtons[param].Name;
             nodeButton.appendChild(nodeButtonImg);
             nodeButton.appendChild(nodeButtonText);
             topMenuElements[i].appendChild(nodeButton);
--- web_script/web_monster.js
+++ web_script/web_monster.js
@@ -7,13 +7,26 @@ module.exports = function(app, tableData, scriptData){
     var route = express.Router();
   
     route.get('/', function (request, response) {
-      var output = '';
-      response.send(output);
+        var monsterTable = tableData['monster'];
+
+        // id값이 존재하는 경우, 상세 페이지로 이동
+        if (request.query.id != undefined && request.query.id != ''){
+          for (var i = 0; i < monsterTable.length; i ++){
+            if (monsterTable[i].ClassID === Number(request.query.id)){
+                monsterDetailPage(i, request, response);
+              return;
+            }
+          }
+        }
+
+        response.send('');
     });
   
     function monsterDetailPage(index, request, response) {
-      var output = '';
-      response.send(output);
+        var monsterTable = tableData['monster'];
+
+        var output = JSON.stringify(monsterTable[index]);
+        response.send(output);
     }
   
     return route;
