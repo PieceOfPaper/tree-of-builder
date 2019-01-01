0e8505da7486c73a72f58aab77cd7e44e25311a0
이성희 <>
2019-01-01 23:17:53 +0900

    퀘스트 페이지 - 잘못된 부분 수정

--- web.js
+++ web.js
@@ -157,11 +157,8 @@ loadTable('monster', 'ies.ipf/monster.ies', function(){
 loadTable('statbase_monster', 'ies.ipf/statbase_monster.ies');
 loadTable('statbase_monster_race', 'ies.ipf/statbase_monster_race.ies');
 loadTable('statbase_monster_type', 'ies.ipf/statbase_monster_type.ies');
-loadTable('questprogresscheck', 'ies.ipf/questprogresscheck.ies', function(){
-  loadTable('questprogresscheck', 'ies.ipf/questprogresscheck_auto.ies', function(){
-  
-  });
-});
+loadTable('questprogresscheck', 'ies.ipf/questprogresscheck.ies');
+loadTable('questprogresscheck', 'ies.ipf/questprogresscheck_auto.ies');
 loadTable('questprogressnpc', 'ies.ipf/questprogressnpc.ies');
 function loadTable(name, path, callback){
   if (tableData[name] === undefined) tableData[name] = [];
