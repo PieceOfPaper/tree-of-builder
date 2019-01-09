16c2e8454c206dd4bc525c325c4e5cae0299a20d
이성희 <>
2019-01-09 16:59:01 +0900

    검색 통합 - 타입 정렬해서 보여주도록 수정

--- data_server/data_ability.js
+++ data_server/data_ability.js
@@ -18,10 +18,31 @@ module.exports = function(app, tableData){
         if (typeList[typeKey] == undefined){
             typeList[typeKey] = [];
             for (var i=0;i<tableData['ability'].length;i++){
+                if (tableData['ability'][i][typeKey] == undefined) continue;
                 if (typeList[typeKey].includes(tableData['ability'][i][typeKey]) == false){
                     typeList[typeKey].push(tableData['ability'][i][typeKey]);
                 }
             }
+            typeList[typeKey].sort(function(a,b){
+                if (a != b){
+                    if (a == undefined) return -1;
+                    else if (b == undefined) return 1;
+                    else {
+                        if ((typeof a)=="number" && (typeof b)=="number"){
+                            if (a < b) return -1;
+                            else return 1;
+                        }
+                        else if ((typeof a)=="string" && (typeof b)=="string"){
+                            return a.localeCompare(b);
+                        }
+                        else if ((typeof a)=="boolean" && (typeof b)=="boolean"){
+                            if (a) return -1;
+                            else return 1;
+                        }
+                    }
+                }
+                return 0;
+            });
         }
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(typeList[typeKey]));
