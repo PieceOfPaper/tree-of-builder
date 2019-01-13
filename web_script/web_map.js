aef6fd94fef25fd022d6401b4dec1559eadb183c
이성희 <>
2019-01-13 19:11:59 +0900

    맵 페이지 - 상세정보 레이아웃 구성

--- web_script/web_map.js
+++ web_script/web_map.js
@@ -25,9 +25,33 @@ module.exports = function(app, tableData, scriptData){
 
     var layout_detail = fs.readFileSync('./web/MapPage/detail.html');
     function mapDetailPage(index, request, response) {
-        var mapTable = tableData['map2'];
-        var output = layout_detail.toString();
-        response.send(output);
+      var mapTable = tableData['map2'];
+      var mapData = mapTable[index];
+
+
+      var output = layout_detail.toString();
+
+      output = output.replace(/%ClassID%/g, mapData.ClassID);
+      output = output.replace(/%ClassName%/g, mapData.ClassName);
+      output = output.replace(/%Name%/g, mapData.Name);
+      output = output.replace(/%MapRank%/g, mapData.MapRank);
+      output = output.replace(/%QuestLevel%/g, mapData.QuestLevel);
+      output = output.replace(/%CategoryName%/g, mapData.CategoryName);
+      output = output.replace(/%Theme%/g, mapData.Theme);
+      output = output.replace(/%Group%/g, mapData.Group);
+      output = output.replace(/%MapType%/g, mapData.MapType);
+      output = output.replace(/%Grimreaper%/g, mapData.Grimreaper);
+      output = output.replace(/%ChallengeMode%/g, mapData.ChallengeMode);
+      output = output.replace(/%Journal%/g, mapData.Journal);
+      output = output.replace(/%WarpCost%/g, mapData.WarpCost);
+      output = output.replace(/%BindCamFarPlane%/g, mapData.BindCamFarPlane);
+      output = output.replace(/%RewardEXPBM%/g, mapData.RewardEXPBM);
+      output = output.replace(/%MaxHateCount%/g, mapData.MaxHateCount);
+      output = output.replace(/%SearchRange%/g, mapData.SearchRange);
+      output = output.replace(/%ChaseRange%/g, mapData.ChaseRange);
+      output = output.replace(/%BornRange%/g, mapData.BornRange);
+
+      response.send(output);
     }
   
     return route;
