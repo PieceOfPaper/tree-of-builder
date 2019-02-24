b6829d413095ef74cfa5945f0b75d6229d4c9574
이성희 <>
2019-02-24 16:46:36 +0900

    비밀먼호 보안 추가

--- web_script/DBPage/web_join.js
+++ web_script/DBPage/web_join.js
@@ -2,7 +2,9 @@ module.exports = function(app, tableData, scriptData, connection){
     var express = require('express');
     var fs = require('fs');
     var mysql = require('mysql');
-    var bodyParser = require('body-parser')
+    var bodyParser = require('body-parser');
+    var md5 = require('md5');
+    var sha256 = require('sha256');
     
     var route = express.Router();
     route.post('/', function (req, res) {
@@ -11,20 +13,31 @@ module.exports = function(app, tableData, scriptData, connection){
         var nickname = req.body.nickname;
 
         var index = 0;
+        var pwd_salt = generateSalt();
 
         connection.query('SELECT * FROM user;', function (error, results, fields) {
             if (error) throw error;
             if (results != undefined && results.length > 0){
                 index = results.length;
             }
+            console.log(sha256(pwd+pwd_salt));
+            connection.query('INSERT INTO user VALUES ('+index+',"'+email+'","'+sha256(pwd+pwd_salt)+'","'+pwd_salt+'","'+nickname+'");', function (error, results, fields) {
+                if (error) throw error;
+            });
+    
+            res.send('<script> window.location.href=".."; </script>');
         });
-
-        connection.query('INSERT INTO user VALUES ('+index+',"'+email+'","'+pwd+'","'+nickname+'");', function (error, results, fields) {
-            if (error) throw error;
-        });
-
-        res.send('<script> window.location.href=".."; </script>');
     });
+
+    function generateSalt(){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=_+~!@#$%^&*()[];,./{}:<>?";
+
+        for (var i = 0; i < 10; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
 
     return route;
 }
--- web_script/DBPage/web_login.js
+++ web_script/DBPage/web_login.js
@@ -2,21 +2,27 @@ module.exports = function(app, tableData, scriptData, connection){
     var express = require('express');
     var fs = require('fs');
     var mysql = require('mysql');
-    var bodyParser = require('body-parser')
+    var bodyParser = require('body-parser');
+    var md5 = require('md5');
+    var sha256 = require('sha256');
     
     var route = express.Router();
     route.post('/', function (req, res) {
         var email = req.body.email;
-        var pwd = req.body.pwd;
 
-        connection.query('SELECT * FROM user WHERE email="'+email+'" AND pwd="'+pwd+'";', function (error, results, fields) {
+        connection.query('SELECT * FROM user WHERE email="'+email+'";', function (error, results, fields) {
             if (error) throw error;
             if (results == undefined || results.length == 0){
-                res.send('<script> alert("No Data"); window.location.href=".."; </script>');
+                res.send('<script> alert("Not Exist User"); window.location.href=".."; </script>');
                 return;
             } else {
-                req.session.login_userno = results[0].userno;
-                res.send('<script> window.location.href=".."; </script>');
+                var pwd = sha256(req.body.pwd + results[0].pwd_salt);
+                if (pwd == results[0].pwd) {
+                    req.session.login_userno = results[0].userno;
+                    res.send('<script> window.location.href=".."; </script>');
+                } else {
+                    res.send('<script> alert("Not Match Password"); window.location.href=".."; </script>');
+                }
             }
         });
 
