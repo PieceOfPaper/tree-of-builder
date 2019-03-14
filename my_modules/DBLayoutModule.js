bfd39dc2ad48122b8395786647e6013a0d33c6c4
이성희 <>
2019-03-14 15:42:17 +0900

    오토 링크 기능 추가

--- my_modules/DBLayoutModule.js
+++ my_modules/DBLayoutModule.js
@@ -1,5 +1,15 @@
 class DBLayoutModule {
 
+    static CommentTextFilter(text) {
+        if (text==undefined) return '';
+        var output = text.toString();
+        var regURL = new RegExp("(http|https|ftp|telnet|news|irc)://([-/.a-zA-Z0-9_~#%$?&=:200-377()]+)","gi");
+        var regEmail = new RegExp("([xA1-xFEa-z0-9_-]+@[xA1-xFEa-z0-9-]+\.[a-z0-9-]+)","gi");
+        output = output.replace(regURL,"<a href='$1://$2' target='_blank'>$1://$2</a>");
+        //output = output.replace(regEmail,"<a href='mailto:$1'>$1</a>");
+        return output;
+    }
+
     static Layout_LoginForm() {
         var output = '';
         output += '<form action="/Login" method="POST" style="margin:0; padding:5px; width:fit-content; display:inline-block; border: 1px solid black;">';
@@ -39,7 +49,7 @@ class DBLayoutModule {
         if (userdata != undefined && userdata.mail_auth != undefined && userdata.mail_auth == "A"){
             output += '<form action="/BoardShort/ReqWrite" method="POST" >';
             output +=  '<input type="hidden" name="userno" value="'+userdata.userno+'" style="margin:2px; width:calc(100% - 4px);" >';
-            output +=  '<input type="normal" name="value" style="margin:2px; width:calc(100vw - 40px);" >';
+            output +=  '<textarea name="value" style="margin:2px; width:calc(100vw - 40px);" ></textarea>';
             output +=  '<button type="submit" style="float:right;">Submit</button>';
             output += '</form>';
             output += '<br>';
@@ -50,7 +60,8 @@ class DBLayoutModule {
                 var date = new Date(results[i].time);
                 output += '<div style="margin-top:10px; margin-bottom:5px; text-align:left;">';
                 output +=  '<p style="margin:2px;"><b>['+results[i].nickname+']</b><span style="float:right; font-size:0.5em;">'+date.toLocaleString()+'</span></p>';
-                output +=  '<p style="margin:2px;">'+results[i].value+'</p>';
+                if (results[i].value!=undefined && results[i].value.length>0)
+                    output +=  '<p style="margin:2px;">'+this.CommentTextFilter(results[i].value)+'</p>';
                 output += '</div>';
             }
         }
