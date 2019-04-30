be18c26a198e0c682b961cd33270c5df231a4e75
이성희 <>
2019-05-01 02:38:19 +0900

    DB 임포트 기능 통합

--- web.js
+++ web.js
@@ -122,8 +122,70 @@ testConnection.connect(function(err) {
   console.log('### DB Connect Success. connected as id ' + testConnection.threadId);
   serverSetting['db-testConnect'] = true;
   testConnection.end();
+  DBImport(serverSetting['dbconfig']);
 });
 //connection.end();
+
+function DBImport(dbconfig){
+  var conns = new mysqls(dbconfig);
+
+  var filelist = undefined;
+  fs.readdir('./table_setting', (err, files) => {
+    if (err) console.error(err);
+    if (files === undefined || files.length == 0){
+      console.log('no setting.')
+      return;
+    }
+    filelist = files;
+    import_db(0);
+    //for (var i=0;i<files.length;i++){
+    function import_db(i){
+      if (i >= filelist.length) return;
+      var table_setting_file = fs.readFileSync('./table_setting/'+filelist[i]);
+      var column_setting = table_setting_file.toString().split('\n');
+      var tablename = filelist[i].split('.')[0];
+
+      var conn = mysql.createConnection(dbconfig);
+      conn.on('error', function() {});
+      conn.connect();
+      //console.log('[DB Import] Check Has Table : ' + tablename);
+      conn.query('SELECT 1 FROM '+tablename+' LIMIT 1;', function (error, results, fields) {
+        conn.end();
+        if (error){
+          //console.log('[DB Import] Check Has Table : ' + tablename + ' => NOT HAS');
+          console.log('[DB Import] Create Table : ' + tablename);
+          var querystr ='CREATE TABLE '+tablename+' (';
+          for (var j=0;j<column_setting.length;j++){
+            if (j>0) querystr +=', '
+            querystr +=column_setting[j];
+          }
+          querystr +=');';
+          conns.query(querystr);
+        } else {
+          //console.log('[DB Import] Check Has Table : ' + tablename + ' => HAS');
+          for (var j=0;j<column_setting.length;j++){
+            if (column_setting[j].trimLeft().startsWith('PRIMARY KEY')){
+              //console.log('[DB Import] Add Primary Key : ' + tablename + ' ' + column_setting[j]);
+              var keyname = column_setting[j].split('(')[1].split(')')[0].trim();
+              conns.query('ALTER TABLE '+tablename+' DROP PRIMARY KEY;');
+              conns.query('ALTER TABLE '+tablename+' ADD PRIMARY KEY ('+keyname+');');
+              continue;
+            }
+            var hasColumn = conns.query('SHOW COLUMNS FROM '+tablename+' LIKE "'+column_setting[j].trimLeft().split(' ')[0]+'";');
+            if (hasColumn!=undefined && hasColumn.length>0){
+              //console.log('[DB Import] Update Column : ' + tablename + ' ' + column_setting[j]);
+              conns.query('ALTER TABLE '+tablename+' MODIFY COLUMN '+column_setting[j]+';');
+            } else {
+              console.log('[DB Import] Add Column : ' + tablename + ' ' + column_setting[j]);
+              conns.query('ALTER TABLE '+tablename+' ADD COLUMN '+column_setting[j]+';');
+            }
+          }
+        }
+        import_db(i + 1);
+      });
+    }
+  });
+}
 
 
 
