f5d2ade23af0dfbe8b937363646cbd5956fba639
이성희 <>
2019-09-15 23:11:20 +0900

    챌린지 계산기 퀘리스트링 기능 추가

--- web_script/web_tool_cmcalulator.js
+++ web_script/web_tool_cmcalulator.js
@@ -6,16 +6,21 @@ module.exports = function(app, serverSetting, serverData){
     
     var route = express.Router();
   
-    var layout = fs.readFileSync('./web/Layout/tool_cmcalulator.html');
+    // var layout = fs.readFileSync('./web/Layout/tool_cmcalulator.html');
     route.get('/', function (request, response) {
         tos.RequestLog(request);
 
-        var output = layout.toString();
-
-        output = output.replace(/%SilverForm%/g, '<input type="checkbox" id="CMSilver_Check" checked>'+tos.GetItemImgString(serverData,'Vis')+' <input type="number" id="CMSilver_Count" value=750000><br/>');
-        output = output.replace(/%PlaniumForm%/g, '<input type="checkbox" id="CMPlanium_Check">'+tos.GetItemImgString(serverData,'misc_Planium')+' <input type="number" id="CMPlanium_Count" value=0.05> <input type="number" id="CMPlanium_Silver" value=2000000><br/>');
-        output = output.replace(/%SierraForm%/g, '<input type="checkbox" id="CMSierra_Check">'+tos.GetItemImgString(serverData,'misc_ore23')+' <input type="number" id="CMSierra_Count" value=15> <input type="number" id="CMSierra_Silver" value=9000><br/>');
-        response.send(output);
+        fs.readFile('./web/Layout/tool_cmcalulator.html', function(err, data){
+            if (err) {
+                response.send('');
+                return;
+            }
+            var output = data.toString();
+            output = output.replace(/%SilverForm%/g, '<input type="checkbox" id="CMSilver_Check" checked>'+tos.GetItemImgString(serverData,'Vis')+' <input type="number" id="CMSilver_Count" value=1><br/>');
+            output = output.replace(/%PlaniumForm%/g, '<input type="checkbox" id="CMPlanium_Check">'+tos.GetItemImgString(serverData,'misc_Planium')+' <input type="number" id="CMPlanium_Count" value=0> <input type="number" id="CMPlanium_Silver" value=0><br/>');
+            output = output.replace(/%SierraForm%/g, '<input type="checkbox" id="CMSierra_Check">'+tos.GetItemImgString(serverData,'misc_ore23')+' <input type="number" id="CMSierra_Count" value=0> <input type="number" id="CMSierra_Silver" value=0><br/>');
+            response.send(output);
+        });
     });
     return route;
 }
